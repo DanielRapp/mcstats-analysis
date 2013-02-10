8b24e0f03a26802af56e89d5d22569e5e008ace1
Daniel Rapp <danielrappt@gmail.com>
2013-02-10 12:16:00 +0100

    Fixed a bug in json2csv

--- json2csv.py
+++ json2csv.py
@@ -6,7 +6,7 @@ with open(sys.argv[1], 'r') as input_file, open(sys.argv[1]+'.csv', 'w') as outp
 	fields = json.loads( input_file.readline() ).keys()
 
 	file_writer = csv.writer(output_file, quoting=csv.QUOTE_NONNUMERIC)
-	file_writer.writerow( fields )
+	file_writer.writerow( list(fields) )
 
 	for line in input_file:
 		try:
