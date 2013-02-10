9d6d8134b211aba02573e71d4b4a9311f9e4ba15
Daniel Rapp <danielrappt@gmail.com>
2013-02-10 14:08:50 +0100

    Re-added json2csv.py, since some (one) seem to have a use for it

---
+++ json2csv.py
@@ -0,0 +1,17 @@
+import json
+import csv
+import sys
+
+with open(sys.argv[1], 'r') as input_file, open(sys.argv[1]+'.csv', 'w') as output_file:
+	fields = json.loads( input_file.readline() ).keys()
+
+	file_writer = csv.writer(output_file, quoting=csv.QUOTE_NONNUMERIC)
+	file_writer.writerow( fields )
+
+	for line in input_file:
+		try:
+			line_json = json.loads(line)
+			values = [line_json[field] for field in fields]
+			file_writer.writerow(values)
+		except:
+			continue
